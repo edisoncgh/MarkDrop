a40f54e00da89a6dc66d596a79c3ac60bec136fa
edisoncgh <cgh@edisoncgh.com>
2026-03-25 15:21:52 +0800

    feat: add homepage view and URL routes

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    # 后续添加路由
+    path('', views.index, name='index'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,15 @@
-"""
-Blog views.
-"""
-# Create your views here.
+from django.shortcuts import render
+from .models import Post, Category, Tag
+
+
+def index(request):
+    """首页视图"""
+    posts = Post.objects.filter(status='published').order_by('-published_at')[:10]
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
+    return render(request, 'pages/index.html', {
+        'posts': posts,
+        'categories': categories,
+        'tags': tags,
+        'site': {'title': 'EDGP 博客', 'description': '基于 Django 的静态博客生成器'}
+    })
